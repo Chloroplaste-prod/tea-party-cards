f8df7a5e863ca940406c904851a8cb16d15df156
Chloroplaste-prod <chloroplaste.productions@gmail.com>
2025-05-19 14:05:55 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,32 @@
-// List of your cards:
 const cards = [
-    {
-        image: 'cards/king.png',
-        title: 'The Elegant Fox',
-        description: 'Dress in something orange or amber. Bring homemade scones or carrot cake.'
-    },
-    {
-        image: 'cards/queen.png',
-        title: 'The Velvet Crow',
-        description: 'Wear black velvet or dark feathers. Bring dark chocolate or black tea.'
-    },
-    {
-        image: 'cards/7.png',
-        title: 'The Royal Rose',
-        description: 'Dress in florals or pink. Bring rose macarons or a strawberry tart.'
-    }
-    // ... add more cards here as needed
+  {
+    image: 'cards/king.png',
+    title: 'The Elegant Fox',
+    description: 'Dress in something orange or amber. Bring homemade scones or carrot cake.'
+  },
+  {
+    image: 'cards/queen.png',
+    title: 'The Velvet Crow',
+    description: 'Wear black velvet or dark feathers. Bring dark chocolate or black tea.'
+  },
+  {
+    image: 'cards/7.png',
+    title: 'The Royal Rose',
+    description: 'Dress in florals or pink. Bring rose macarons or a strawberry tart.'
+  }
+  // ...add more cards
 ];
 
 const drawBtn = document.getElementById('drawBtn');
 const cardDisplay = document.getElementById('cardDisplay');
 
 drawBtn.addEventListener('click', () => {
-    // Pick a random card
-    const card = cards[Math.floor(Math.random() * cards.length)];
-    cardDisplay.innerHTML = `
+  const card = cards[Math.floor(Math.random() * cards.length)];
+  cardDisplay.innerHTML = `
     <img class="card-img" src="${card.image}" alt="${card.title}">
     <div class="card-title">${card.title}</div>
     <div class="card-desc">${card.description}</div>
   `;
-    drawBtn.disabled = true; // Only allow one draw
-    drawBtn.textContent = "Card Drawn!";
+  drawBtn.disabled = true;
+  drawBtn.textContent = "Card Drawn!";
 });
